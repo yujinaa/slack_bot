65efc80a180e20b92fb1b5108ff3cc649e8dfc65
yujin Na <olim920@naver.com>
2023-04-23 23:39:20 +0900

    slack API client로 메세지보내기

--- slackBot_test/src/main/java/com/care/root/slackMessage.java
+++ slackBot_test/src/main/java/com/care/root/slackMessage.java
@@ -1,24 +1,9 @@
 package com.care.root;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
-
 import com.slack.api.Slack;
-import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
-
 
 public class slackMessage {
 	public static void main(String[] args) throws IOException, SlackApiException {
@@ -26,15 +11,14 @@ public class slackMessage {
 		sendTextMessage();
 	}	
 
-		private static String token = "";
-	//	private static String channel = "C0536F20X7T";
+	private static Slack slack = Slack.getInstance();
+	private static String token = "토큰";
 
-		public static void sendTextMessage() throws IOException, SlackApiException {
-			Slack slack = Slack.getInstance();
-//			String token = System.getenv("");
+	public static void sendTextMessage() throws IOException, SlackApiException {
+		//		Slack slack = Slack.getInstance();  Slack 개체 초기화로 슬랙 시작을 위한 코드 
 
 		ChatPostMessageResponse response = slack.methods(token).chatPostMessage(req -> req
 				.channel("") // Channel ID
-				.text(":cat: Hi from a bot written in Java!"));
+				.text(":wave: Hi from a bot written in Java!"));
 	}
 }
