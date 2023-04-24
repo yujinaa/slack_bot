1eecc379534491cbea24eff38e859fcda098fc9e
yujin Na <olim920@naver.com>
2023-04-25 01:53:31 +0900

    스케줄러로 특정 시간에 메세지 보내기

--- slackBot_test/src/main/java/com/care/root/schedule.java
+++ slackBot_test/src/main/java/com/care/root/schedule.java
@@ -1,6 +1,5 @@
 package com.care.root;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
